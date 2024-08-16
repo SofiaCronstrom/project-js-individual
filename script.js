6940c0a68ed034c47b45d25b43c7c5e0a82169c8
Sofia <sofia.cronstrom@gmail.com>
2024-08-16 16:32:20 +0200

    Beginning on CRUD with local storage at script.js

--- script.js
+++ script.js
@@ -2,35 +2,77 @@
     const searchInput = document.querySelector('search-input');
     const searchButton = document.querySelector('search-button');
     const searchResults = document.querySelector('.search-results');
-    const library = document.querySelector('.art-favorites');
+    const catCollection = document.querySelector('.art-favorites');
     
     // Local Storage Key
-    const STORAGE_KEY = 'artCollection';
+    const CAT_KEY = 'catCollection';
 
     // Fetch books from local storage
-    function getLibraryFromLocalStorage() {
-        return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+    function getCollectionFromLocalStorage() {
+        return JSON.parse(localStorage.getItem(CAT_KEY)) || [];
     }
 
     // Save books to local storage
-    function saveLibraryToLocalStorage(books) {
+    function saveCollectionToLocalStorage(books) {
         localStorage.setItem(STORAGE_KEY, JSON.stringify(books));
     }
-    
-    ArtImg();
 
-     async function ArtImg() {
-           let arts = await fetchArt();
+    //Render library
+    function renderCollection() {
+      let cats = getCollectionFromLocalStorage();
+      
+      //sort pinned images
+      cats = cats.sort((a, b) =>(b.pinned ? 1 : 0) - (a.pinned ? 1 : 0));
+
+      catCollection.innerHTML = '';
+      cats.forEach((cat, index) => {
+       
+        catCollection.innerHTML +=
+        `<figure>
+        <img src='${cat.url}'/>
+        <button data-index="${index}" class="pin-btn">
+        ${cat.pinned ? 'Unpin' : 'Pin'}
+        </button>
+        <button data-index="${index}" class="remove-btn">
+        Remove
+        </button>
+        </figure>`;
+        
+      });
+    }
+    
+    //Add image to cat collection
+    function addCat (image){
+    
+      const cats = getCollectionFromLocalStorage();
+      cats.push({ image, pinned: false });
+      saveCollactionToLocalStorage(books);
+      renderLibrary();
+ 
+    }
+    
+    // Toggle the pinned status
+    function togglePinnedImg(index) {
+      const cats = getCollectionFromLocalStorage();
+      cats[index].pinned = !cats[index].pinned; 
+      saveCollectionToLocalStorage(books);
+      renderCollection();
+  }
+
+    ArtImg();
+     //Fetch random 10 cat images
+     async function catImg() {
+           let cats = await fetchCat();
           
-          arts.forEach(art => {
+          cats.forEach(cat => {
             searchResults.innerHTML += `  <figure>
-                                           <img src='${art.url}'/>
-                                          <button class="save-button">Save</button>
+                                           <img src='${cat.url}'/>
+                                          <button data-title="${cat.url}"class="save-button">Save</button>
                                           </figure>`
           });
          }
 
-    async function fetchArt() {
+    async function fetchCat() {
    try {
      let response = await fetch("https://api.thecatapi.com/v1/images/search?limit=9");
      let data = await response.json();
